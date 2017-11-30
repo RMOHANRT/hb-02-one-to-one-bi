41bcc25c339c08ef097d0ab73c225dc09ddd6759
RMOHANRT <rmohanr.t@gmail.com>
2017-11-30 07:53:16 +0530

    Initial commit

---
+++ src/com/luv2code/hibernate/demo/DeleteDetailDemo.java
@@ -0,0 +1,66 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteDetailDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+	
+			session.beginTransaction();
+			
+			int theId = 2;
+			
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class ,theId);
+			
+			System.out.println("Instructor Details"+tempInstructorDetail);
+
+			//associated instrctor details.
+			
+			System.out.println("the associated Instrctor"+tempInstructorDetail.getInstructor());
+			
+			
+			System.out.println("deleted Instrctor details"+tempInstructorDetail);
+			
+			
+			session.delete(tempInstructorDetail);
+			// save the student object
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			
+			session.close();
+			factory.close();
+			
+		}
+	}
+
+}
+
+
+
+
+
---
+++ src/com/luv2code/hibernate/demo/DeleteInstructorDemo.java
@@ -0,0 +1,64 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteInstructorDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			// create a student object
+			System.out.println("Creating new student object...");
+
+			// start a transaction
+						session.beginTransaction();
+			int theId = 1;
+			
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("Instructor Details"+tempInstructor);
+			
+			if(tempInstructor != null) {
+			
+				System.out.println("deleting Instructor Details"+tempInstructor);
+				
+				session.delete(tempInstructor);
+
+			}
+			
+			
+			
+			// save the student object
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
